0ee0db4aee74e944e596b14845fce33358f48c77
yetis_batuhan <batuhanyetis94@gmail.com>
2023-08-15 02:04:18 +0300

    bootstrap added

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-import  express  from "express";
-import  bodyParser from "body-parser";
+import express from "express";
+import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
-app.use(express.static('public'));
-const weatherKey ="2a09ebb1f33ad2ac1877c2a1e7ad3113";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// express.static middleware'ini önce eklemelisiniz
+
+const weatherKey = "2a09ebb1f33ad2ac1877c2a1e7ad3113";
 app.get("/", (req, res) => {
-    
-    
-    
-    res.render("index.ejs",);
-  });
+    res.render("index.ejs");
+});
 
-
-
+app.post("/", (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+});
