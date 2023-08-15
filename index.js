3441a2dea48388c97510a824167162355ab248c9
yetis_batuhan <batuhanyetis94@gmail.com>
2023-08-16 01:26:08 +0300

    index.js current-apı and geomap apı prepared

--- index.js
+++ index.js
@@ -14,10 +14,22 @@ const weatherKey = "2a09ebb1f33ad2ac1877c2a1e7ad3113";
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
+async function calc(city){
 
-app.post("/", (req, res) => {
-    console.log(req.body.city)
+}
+app.post("/",async (req, res) => {
+    const city = req.body.city ;
+    const geoMap = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${weatherKey}`)
+    const cityLat= JSON.stringify(geoMap.data[0].lat);
+    const cityLon= JSON.stringify(geoMap.data[0].lon);
+    const cityLatFixed = parseFloat(cityLat).toFixed(2);
+    const cityLonFixed = parseFloat(cityLon).toFixed(2);
+    const current = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLatFixed}&lon=${cityLonFixed}&appid=${weatherKey}`)
+    console.log(current.data);
+    
+    
     res.render("index.ejs");
+    
 });
 
 app.listen(port, () => {
