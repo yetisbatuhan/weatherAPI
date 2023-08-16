50272c8602caa96879b1ed657e673908a3af3a70
yetis_batuhan <batuhanyetis94@gmail.com>
2023-08-16 16:33:23 +0300

    indexjs ok but index.ejs will changed again

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ const weatherKey = "2a09ebb1f33ad2ac1877c2a1e7ad3113";
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
-async function calc(city){
 
-}
+
+
 app.post("/",async (req, res) => {
     const city = req.body.city ;
     const geoMap = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${weatherKey}`)
-    const cityLat= JSON.stringify(geoMap.data[0].lat);
+    const cityName = geoMap.data[0].name
     const cityLon= JSON.stringify(geoMap.data[0].lon);
+    const cityLat= JSON.stringify(geoMap.data[0].lat);
     const cityLatFixed = parseFloat(cityLat).toFixed(2);
     const cityLonFixed = parseFloat(cityLon).toFixed(2);
     const current = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLatFixed}&lon=${cityLonFixed}&appid=${weatherKey}`)
-    console.log(current.data);
-    
-    
-    res.render("index.ejs");
+    const celciusNow = Math.floor(current.data.main.temp-273,15)
+    console.log(current.data)
+    res.render("index.ejs",{city: cityName,celcius: celciusNow,topLane: current.data,});
     
 });
 
